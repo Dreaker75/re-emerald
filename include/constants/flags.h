6edbda6ccaa5dacbc01857e1d5e2b6bf15541b21
Dreaker <dreakerguides@gmail.com>
2024-07-27 20:41:01 +0200

    The Lati on Souther Island will only hold the Soul Dew on the first fight

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -512,7 +512,7 @@
 #define FLAG_DEFEATED_HO_OH                  0x1DC
 #define FLAG_DEFEATED_LUGIA                  0x1DD
 
-#define FLAG_UNUSED_0x1DE                    0x1DE // Unused Flag
+#define FLAG_FOUGHT_LATIOS_OR_LATIAS_SOUTHERN_BEFORE   0x1DE // Unused Flag
 #define FLAG_UNUSED_0x1DF                    0x1DF // Unused Flag
 #define FLAG_UNUSED_0x1E0                    0x1E0 // Unused Flag
 #define FLAG_UNUSED_0x1E1                    0x1E1 // Unused Flag
